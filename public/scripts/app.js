db2107901690b75cf6f3b06797e6f766490fad88
tbduong <tiffaniduong@gmail.com>
2016-07-18 15:52:38 -0700

    seed data now able to render on page. created find function within albumController

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,30 +9,7 @@ var $albumsList;
 var sampleAlbums = [];
 
 /* hard-coded data! */
-sampleAlbums.push({
-  artistName: 'Ladyhawke',
-  name: 'Ladyhawke',
-  releaseDate: '2008, November 18',
-  genres: [ 'new wave', 'indie rock', 'synth pop' ]
-});
-sampleAlbums.push({
-  artistName: 'The Knife',
-  name: 'Silent Shout',
-  releaseDate: '2006, February 17',
-  genres: [ 'synth pop', 'electronica', 'experimental' ]
-});
-sampleAlbums.push({
-  artistName: 'Juno Reactor',
-  name: 'Shango',
-  releaseDate: '2000, October 9',
-  genres: [ 'electronic', 'goa trance', 'tribal house' ]
-});
-sampleAlbums.push({
-  artistName: 'Philip Wesley',
-  name: 'Dark Night of the Soul',
-  releaseDate: '2008, September 12',
-  genres: [ 'piano' ]
-});
+
 /* end of hard-coded data */
 
 $(document).ready(function() {
@@ -53,11 +30,6 @@ function onSuccess(json) {
         renderAlbum(album);
     });
 }
-
-
-
-
-
 
 // this function takes a single album and renders it to the page
 function renderAlbum(album) {
--- seed.js
+++ seed.js
@@ -1,9 +1,35 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
-
 var db = require("./models");
 
 var albumsList =[
+  {
+    artistName: 'Nine Inch Nails',
+    name: 'The Downward Spiral',
+    releaseDate: '1994, March 8',
+    genres: [ 'industrial', 'industrial metal' ],
+  },
+
+  {
+    artistName: 'Metallica',
+    name: 'Metallica',
+    releaseDate: '1991, August 12',
+    genres: [ 'heavy metal' ],
+  },
+
+  {
+    artistName: 'The Prodigy',
+    name: 'Music for the Jilted Generation',
+    releaseDate: '1994, July 4',
+    genres: [ 'electronica', 'breakbeat hardcore', 'rave', 'jungle' ]
+  },
+  {
+    artistName: 'Johnny Cash',
+    name: 'Unchained',
+    releaseDate: '1996, November 5',
+    genres: [ 'country', 'rock' ]
+  }
+
   // put data here!
 ];
 //
